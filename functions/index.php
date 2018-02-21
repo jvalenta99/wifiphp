450d7f1f65944e10239ce2ed4f4c635dba4deb0c
jvalenta99 <32281373+jvalenta99@users.noreply.github.com>
2018-02-21 10:53:33 +0100

    dsfgh

---
+++ functions/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Functions</title>
+</head>
+<body>
+    <h1>Functions</h1>
+    <?php
+        /* 
+        
+        */
+
+        function firstFunction(){
+            echo 'i bins,dei Funktion';
+        }
+
+        firstFunction();
+    ?>
+    
+</body>
+</html>
