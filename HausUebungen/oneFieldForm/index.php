bbfd06a26f42053b6155de6bae7353892c337f2a
Jiri Valenta <jvalenta99@gmail.com>
2018-02-27 22:28:19 +0100

    asdf

---
+++ HausUebungen/oneFieldForm/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Einfeld Formular</title>
+    <link rel="stylesheet" href="css/pure-min.css">
+    <link rel="stylesheet" href="css/layout.css">
+</head>
+<body>
+    <header class="site-header">
+        <h1>Einfachformular mit Kontrolle</h1>
+    </header>
+    <main>
+        <h2>Bitte ausfüllen:</h2>
+        <form action="" method="post" class="pure-form pure-form-stacked">
+        <label for="feld">Check field</label>
+        <input type="text" name="feld" id="feldid">
+
+        <input type="submit" value="Senden" class="pure-button pure-button-primary">
+        </form>
+    </main>
+    
+</body>
+</html>
