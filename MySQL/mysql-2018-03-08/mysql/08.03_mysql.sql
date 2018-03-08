63045223a7392b111669849401a40e67a6107216
jvalenta99 <32281373+jvalenta99@users.noreply.github.com>
2018-03-08 17:02:36 +0100

    gfhj

--- MySQL/mysql-2018-03-08/mysql/08.03_mysql.sql
+++ MySQL/mysql-2018-03-08/mysql/08.03_mysql.sql
@@ -1,23 +1,26 @@
 create database wifitag3 charset utf8 collate utf8_unicode_ci;
 use wifitag3;
 
-drop table if exists Ort;
 drop table if exists Kunden;
-create table Ort ( ortId INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-						         ortName varchar(100) not null) engine InnoDB character set utf8 collate utf8_unicode_ci;
+drop table if exists Ort;
+
+create table Ort ( 
+	ortId INT unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	ortName varchar(100) not null
+) engine InnoDB character set utf8 collate utf8_unicode_ci;
                                 
                                 
-	   insert into Ort (ortName) values
-                        
-	                     ('Wien'),('St.Pölten');
-                         
-                         select *from Ort;
-create table Kunden
-     (kundId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-						kundName varchar(100) not null ,
-                        ortId int unsigned,
-                        constraint orfkx foreign key (ortId) references Ort (ortId)
-                        on delete cascade on update cascade) engine InnoDB character set utf8 collate utf8_unicode_ci;
+insert into Ort (ortName) values ('Wien'),('St.Pölten');
+select *from Ort;
+
+create table Kunden(
+	kundId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	kundName varchar(100) not null ,
+	ortId int unsigned,
+    constraint orfkx foreign key (ortId) references Ort (ortId)
+    on delete cascade -- restrict, cascade, set null, no action
+    on update cascade -- restrict, cascade, set null, no action
+) engine InnoDB character set utf8 collate utf8_unicode_ci;
                         
                         
 	   insert into Kunden (kundName, ortId) values ('Peter',1);
@@ -40,5 +43,5 @@ create table Kunden
        
        
        update Ort set ortId = 1 where ortId = 10;
-       select * from Kunden;
+       delete from Ort where ortId = 1;
        
